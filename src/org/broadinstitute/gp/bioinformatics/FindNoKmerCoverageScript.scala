1f5da2c3a1059c09e967ae065610d80ad1c8ed0b
Yossi Farjoun <farjoun@broadinstitute.org>
2013-12-18 16:44:55 -0500

    fixed dealing with -xi

--- src/org/broadinstitute/gp/bioinformatics/FindNoKmerCoverageScript.scala
+++ src/org/broadinstitute/gp/bioinformatics/FindNoKmerCoverageScript.scala
@@ -95,10 +95,11 @@ class FindNoKmerCoverageScript extends QScript {
     //write input interval as interval_list
     val mil=new MergeIntervalLists()
     val ciltr2 = new ConvertIntervalListToR(false)
-    if(interval!=null){
+    if(interval!=null| excludeInterval!=null){
       mil.reference_sequence=referenceFile
       mil.targetIntervalsString:+=interval
       mil.intervalsString:+=interval
+      mil.excludeIntervalsString:+=excludeInterval
       mil.out=tempIntervalFile
       add(mil)
 
